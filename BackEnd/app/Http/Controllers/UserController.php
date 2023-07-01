4491df8300ac12e13c68f16920126db0420c81e6
BhMedRayen <rayenmohamed2310@gmail.com>
2023-07-01 15:23:34 +0100

    function getAllUsers , function getUser , function deleteUser

--- BackEnd/app/Http/Controllers/UserController.php
+++ BackEnd/app/Http/Controllers/UserController.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function getAllUsers()
     {
-        
+        $users = User::all();
+        return response()->json(['data'=>$users],200);
+    }
+    public function getUser(int $id)
+    {
+        $user = User::find($id);
+        if($user==null)
+        {
+            return response()->json(['user'=>'User Not Found'],404);
+
+        }
+        return response()->json(['user'=>$user],200);
+    }
+    public function deleteUser(int $id)
+    {
+        $user = User::find($id);
+        if($user==null)
+        {
+            return response()->json(['user'=>'User Not Found'],404);
+
+        }
+        $user->delete();
+        return response()->json(['Message'=>'User Deleted'],200);
+    }
+    public function addUser(Request $request)
+    {
+        $user=new User();
     }
 }
--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/users',[UserController::class,'getAllUsers']);
+Route::get('/user/{id}',[UserController::class,'getUser']);
+Route::delete('/user_delete/{id}',[UserController::class,'deleteUser']);
