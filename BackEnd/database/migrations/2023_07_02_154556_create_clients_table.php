4b51f81d4b6d4aa4806f8d075381dabebf36f468
BhMedRayen <rayenmohamed2310@gmail.com>
2023-07-02 17:45:46 +0100

    Add Migration , model seeders and factories on table client

---
+++ BackEnd/database/migrations/2023_07_02_154556_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            // $table->id();
+            // $table->string('nom');
+            // $table->string('prenom');
+            // $table->string('user_name');
+            // $table->mail('e_mail');
+            // $table->string('password');
+            // $table->unsignedBigInteger('post_id');
+            // $table->timestamps();
+            // $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
