c5f391c01c176f603a5c13941695f315c25bb0a4
BhMedRayen <rayenmohamed2310@gmail.com>
2023-09-26 12:40:42 +0100

    Navbar code beauti

--- frontend/src/components/Profile/NavbarComponent.vue
+++ frontend/src/components/Profile/NavbarComponent.vue
@@ -1,46 +1,28 @@
 <template>
     <div>
-        <v-app-bar
-            color="white"
-       >
-      <v-toolbar-title @click="ChangeSettiing()">
-         <img src="../../assets/logo/TF.png" width="100px">
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
+        <v-app-bar color="white">
+          <v-toolbar-title @click="ChangeSettiing()">
+            <img src="../../assets/logo/TF.png" width="100px">
+          </v-toolbar-title>
+        <v-spacer></v-spacer>
       <div class="d-flex ">
-        <v-btn class="mx-4" color="#E84C03" >
-          <v-icon color="white" @click="editPageAccueil(true)">mdi-home</v-icon>
-        </v-btn>
-        <v-btn class="mx-4"  color="#E84C03">
-          <v-icon color="white"  @click="editPageAccueil(false)">mdi-account</v-icon>
-        </v-btn>
+          <v-btn class="mx-4" color="#E84C03" >
+            <v-icon color="white" @click="editPageAccueil(true)">mdi-home</v-icon>
+          </v-btn>
+          <v-btn class="mx-4"  color="#E84C03">
+            <v-icon color="white"  @click="editPageAccueil(false)">mdi-account</v-icon>
+          </v-btn>
       </div>
             <v-spacer></v-spacer>
-            <v-btn
-                    plain
-                    text @click="ChangeSettiing('chat')">
-                    
+              <v-btn plain text @click="ChangeSettiing('chat')"> 
                        <v-icon  color="green" size="35px">mdi-chat</v-icon>
-                     <v-badge color="red" content="5"
-                       ></v-badge>
-    </v-btn>
-      <v-menu offset-y   
-               transition="slide-x-transition" 
-               left
-               class="mt-7"
-               max-width="auto"
-               min-width="300"
-               >
-               
+                       <v-badge color="red" content="5"></v-badge>
+              </v-btn>
+      <v-menu offset-y transition="slide-x-transition" left class="mt-7" max-width="auto" min-width="300">    
                    <template v-slot:activator="{ on, attrs }">
-                    <v-btn
-                    plain
-                    v-bind="attrs"
-                    v-on="on"
-                    >
+                    <v-btn plain v-bind="attrs" v-on="on">
                        <v-icon color="red" size="35px ">mdi-bell</v-icon>
-                     <v-badge color="red" :content="notifications.length==0 ? '0' : notifications.length"
-                       ></v-badge>
+                     <v-badge color="red" :content="notifications.length==0 ? '0' : notifications.length"></v-badge>
                     </v-btn>
                    </template>
                       <v-list>
@@ -53,11 +35,9 @@
                    <v-list-item  class="mt-5 red--text">
                        <v-list-item-title class="text-h7 ml-8 mb-8">No notifications is available</v-list-item-title>
                    </v-list-item>
-                  </v-list>
+                   </v-list>
                    <v-list v-else>
-                   <v-list-item
-                      v-for="notif in notifications" :key="notif.id" 
-                   >
+                   <v-list-item v-for="notif in notifications" :key="notif.id">
                        <v-list-item-content>
                            <v-list-item-title class="px-5 justify-center">
                              {{ notif.message }}  
@@ -65,11 +45,7 @@
                        </v-list-item-content> 
                        <v-list-item-action>
                            <v-col cols="auto">
-                                <v-btn
-                                 color="red"
-                                 plain
-                                   @click="deleteNotificationById(notif.id)"
-                                   >
+                                <v-btn color="red" plain @click="deleteNotificationById(notif.id)">
                                        <v-icon>mdi-delete</v-icon>
                                    </v-btn>
                            </v-col>
@@ -78,57 +54,40 @@
                        <v-divider></v-divider>
                    </v-list>
                </v-menu>
-    <v-menu class="mx-4"
-       offset-y
-      :nudge-width="100"
-    >
+    <v-menu class="mx-4" offset-y :nudge-width="100">
       <template v-slot:activator="{ attrs, on }">
-        <v-list-item-avatar  v-bind="attrs"
-          v-on="on">
+        <v-list-item-avatar  v-bind="attrs" v-on="on">
                <v-avatar size="43px">
                       <v-img  :src="'http://localhost:8000'+User['Photo']"></v-img>
                 </v-avatar>
        </v-list-item-avatar>
-       
       </template>
-
       <v-list>
-       
-        <v-list-item @click="ChangeSettiing('setting')"
-        >
-        
+        <v-list-item @click="ChangeSettiing('setting')">
             <v-list-item-content >
               <v-list-item-title>Settings</v-list-item-title>
             </v-list-item-content>
             <v-list-item-action>
-              <v-btn text
-              >
+              <v-btn text>
                 <v-icon>mdi-cog</v-icon>
               </v-btn>
             </v-list-item-action>
-        
         </v-list-item>
-        <v-list-item @click="logout()"
-        >
-        
+        <v-list-item @click="logout()">
             <v-list-item-content>
               <v-list-item-title>Logout</v-list-item-title>
             </v-list-item-content>
             <v-list-item-action>
-              <v-btn text
-              >
+              <v-btn text>
                 <v-icon>mdi-logout</v-icon>
               </v-btn>
-            </v-list-item-action>
-        
+            </v-list-item-action> 
         </v-list-item>
       </v-list>
     </v-menu>
- 
     </v-app-bar>
     </div>
 </template>
-
 <script>
 import notiifSerice from "../../Services/Notif.js"
 import {AuthStore} from "../../store/index"
